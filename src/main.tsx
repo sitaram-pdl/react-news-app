931635c53b77f6ef175ea7bfb06e0632b9e40f85
Sitaram <spoudel646@gmail.com>
2025-02-18 19:12:30 +0545

    fix : tailwind and shadecn issue

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,11 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import './index.css';
-import { ThemeProvider } from './context/theme-provider';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
